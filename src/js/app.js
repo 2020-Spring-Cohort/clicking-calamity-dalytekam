ea4e0bb114641cda6d833b4daffc0aad41f9789f
daly richy <dalyeu@yahoo.fr>
2020-03-04 15:04:51 -0500

    Added some tests

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,33 @@
-const username = document.querySelector("#userName");
-
+/*const username = document.querySelector("#userName");
+let isBig = false;
 const usernameEntered = prompt("Please Provide Your User Name");
 
 username.innerText = usernameEntered;
+let numberOfCookies = 0;
+let scoreHolder = document.querySelector("#score");
+const picture = document.querySelector("#pic");
+const cookieHolder = document.querySelector("#cookie");
+const clickSound = document.querySelector("#audio");
+
+
+picture.addEventListener("click", function() {
+
+
+    if (isBig == false) {
+        this.style.transform = "scale(1.2)";
+
+
+        this.style.transition = "transform 100ms linear";
+    } else {
+        this.style.transform = "scale(1)";
+
+        this.style.transition = "transform 100ms linear";
+    }
+    isBig = !isBig;
+
+    numberOfCookies++;
+    scoreHolder.innerText = numberOfCookies;
+    cookieHolder.innerText = numberOfCookies > 1 ? "Cookies" : "Cookie";
+    clickSound.volume = 3;
+    clickSound.play();
+});*/
